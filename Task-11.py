f1f07d2c50bcdeda01d02702eb28cc651ec48a87
igranoboy <igranoboy-22@edu.ranepa.ru>
2023-05-13 13:58:04 +0300

    Edit_1

--- Task-11.py
+++ Task-11.py
@@ -2,6 +2,7 @@ class Dog:
     def __init__(self, name, breed, age):
         self.name = name
         self.breed = breed
-        self.age = age
+        self.age = 0
+
     def print_information(self):
-        print('Имя: ', self.name, 'Порода: ', self.breed, 'Возраст: ', self.age)
+        print('Имя: ', self.name, ', порода: ', self.breed, ', возраст: ', self.age)
