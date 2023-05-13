49749888b8382e06da547844a251b281fe79c784
igranoboy <igranoboy-22@edu.ranepa.ru>
2023-05-13 13:23:46 +0300

    Done

---
+++ Task-9.py
@@ -0,0 +1,8 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = 0
+        self.width = 0
+    def square(length, width):
+        square = length * width
+        return square
+
