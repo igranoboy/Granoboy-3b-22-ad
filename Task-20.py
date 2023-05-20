00a6a3810b92864c398d6a64ad465f212f614da5
igranoboy <igranoboy-22@edu.ranepa.ru>
2023-05-20 16:56:07 +0300

    Edit_1

--- Task-20.py
+++ Task-20.py
@@ -5,14 +5,7 @@ for i in range(10):
     my_list.append(random.randrange(1, 101))
 print('Массив: ', my_list)
 
-maximum = my_list[0]
-for i in range(9):
-    if maximum < my_list[i + 1]:
-        maximum = my_list[i + 1]
-print('Максимальное значение: ', maximum)
+my_list.sort()
+print('Минимальное значение: ', my_list[0])
+print('Максимальное значение: ', my_list[9])
 
-minimum = my_list[0]
-for i in range(9):
-    if minimum > my_list[i + 1]:
-        minimum = my_list[i + 1]
-print('Минимальное значение: ', minimum)
---
+++ Task-21.py
@@ -0,0 +1,5 @@
+list_1 = [1, 2, 3]
+list_2 = [4, 5, 6]
+new_list = []
+new_list.extend([list_1, list_2])
+print(new_list)
