1033e6b4eb61d15432915e16170fa6792e9debda
igranoboy <igranoboy-22@edu.ranepa.ru>
2023-05-20 21:26:58 +0300

    Done

--- Task-28.py
+++ Task-28.py
@@ -1,1 +1,20 @@
-#5
+def calculator(a, b, operator):
+    if operator == '+':
+        result = a + b
+    elif operator == '-':
+        result = a - b
+    elif operator == '*':
+        result = a * b
+    elif operator == '/':
+        if b != 0:
+            result = a / b
+        else:
+            print('На 0 делить нельзя')
+            return
+    print(result)
+
+num_1 = float(input('Введите первое число: '))
+action = input('Выберете действие (+, -, *, /): ')
+num_2 = float(input('Введите второе число: '))
+
+calculator(num_1, num_2, action)
